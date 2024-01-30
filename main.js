cbeae30f588fe12f0a756df2daf1cc0b0f7cbfcc
Brady Norton <bnort.11@gmail.com>
2024-01-30 09:43:15 -0500

    Added multiple sections and fixed some bugs: - Added pricing section - Added location section - Added certification section - Fixed some image bugs - Updated Owner section - Some style changes

--- main.js
+++ main.js
@@ -7,3 +7,41 @@ navToggle.addEventListener('click', () => {
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+// Pricing Code
+const tabsContainer = document.querySelector(".tabs-container");
+const tabsList = tabsContainer.querySelector("ul");
+const tabButtons = tabsList.querySelectorAll("a");
+const tabPanels = tabsContainer.querySelectorAll(".tabs__panels > div");
+
+tabButtons.forEach((tab, index) => {
+    if(index === 0) {
+        //
+    } else {
+        tabPanels[index].setAttribute("hidden","");
+    }
+});
+
+tabsContainer.addEventListener("click", (e) => {
+    const clickedTab = e.target.closest("a");
+    if(!clickedTab) return;
+    e.preventDefault();
+
+    const activePanelId = clickedTab.getAttribute('href');
+    const activePanel = tabsContainer.querySelector(activePanelId);
+
+    tabPanels.forEach((panel) => {
+        panel.setAttribute("hidden", true);
+    });
+    activePanel.removeAttribute("hidden", false);
+});
+
+// Pricing Buttons
+const btnList = document.querySelectorAll('.pricing-button');
+
+btnList.forEach(btnEl => {
+    btnEl.addEventListener('click', () => {
+        document.querySelector('.selected')?.classList.remove('selected');
+        btnEl.classList.add('selected');
+    });
+});
